96a9f7b794292d51c33933ec2741f3e7f184f9c2
sirozza <s.matviv@gmail.com>
2023-12-16 12:54:20 +0200

    leet-161223-01

---
+++ test/test01.py
@@ -0,0 +1,18 @@
+
+def decor(func):
+    def wrapper(*args):
+        func(*args)
+        print(f'Внутри Декоратора')
+    return wrapper
+
+@decor
+def foo():
+    return print('Это работает func()')
+
+def foo2(a=None):
+    print(a)
+
+
+if __name__ == '__main__':
+    foo2(a=[1, 2, 3])
+    foo2(['a', 'b'])
