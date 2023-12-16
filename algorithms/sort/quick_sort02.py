65a9eefc96529ce2389cca7a0bc558b97f890141
sirozza <s.matviv@gmail.com>
2023-12-16 22:01:27 +0200

    leet_161223

---
+++ algorithms/sort/quick_sort02.py
@@ -0,0 +1,23 @@
+from random import choice
+
+
+s = [3, 0, 7, 1, 5, 2]
+
+print(s)
+
+def quick_soft(s):
+    if len(s)<=1:
+        return s
+    q = choice(s)
+    l, m, r = [], [], []
+
+    for x in s:
+        if x < q:
+            l.append(x)
+        elif x > q:
+            r.append(x)
+        else:
+            m.append(x)
+    return  quick_soft(l) + m + quick_soft(r)
+
+print(quick_soft(s))
---
+++ algorithms/sort/quick_sort03.py
@@ -0,0 +1,23 @@
+from random import randint
+
+def quicksort(a, fst, lst):
+    if fst >= lst:
+        return
+    i, j = fst, lst
+    pivot = a[randint(fst, lst)]
+    while i <= j:
+        while a[i] < pivot:
+            i += 1
+        while a[j] > pivot:
+            j -= 1
+        if i <= j:
+            a[i], a[j] = a[j], a[i]
+            i, j = i + 1, j -1
+    quicksort(a, fst, j)
+    quicksort(a, i, lst)
+
+a = [10, -2, 1, 5, 0, 25, 3, 4]
+
+quicksort(a, 0, len(a)-1)
+
+print(a)
