271cd4e8664d483215ce42274b017371d56a56cf
sirozza <s.matviv@gmail.com>
2024-12-23 19:05:09 +0200

    231224_04

---
+++ 14. Longest Common Prefix/01.py
@@ -0,0 +1,28 @@
+# If
+# there is no
+# common
+# prefix,
+# return an
+# empty
+# string
+# "".
+#
+# Example
+# 1:
+#
+# Input: strs = ["flower", "flow", "flight"]
+# Output: "fl"
+#
+# Example
+# 2:
+#
+# Input: strs = ["dog", "racecar", "car"]
+# Output: ""
+
+
+strs = ["flower", "flow", "flight"]
+
+pef = ''
+
+while True:
+    
