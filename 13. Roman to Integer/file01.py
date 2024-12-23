e105ce14e73869b93e386136e11866f9e743888a
sirozza <s.matviv@gmail.com>
2024-12-23 10:30:05 +0200

    231224_01

--- 13. Roman to Integer/file01.py
+++ 13. Roman to Integer/file01.py
@@ -40,5 +40,29 @@
 #     It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 
 
-class Solution:
-    def romanToInt(self, s: str) -> int:
+# class Solution:
+#     def romanToInt(self, s: str) -> int:
+
+def romanToInt(s: str) -> int:
+    res = 0
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    for a, b in zip(s, s[1:]):
+        if roman[a] < roman[b]:
+            res -= roman[a]
+            print(a)
+        else:
+            res += roman[a]
+            print(a)
+
+    return res + roman[s[-1]]
+a = 'C'
+print(romanToInt(a))
