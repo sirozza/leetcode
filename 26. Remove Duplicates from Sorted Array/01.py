7db8bebf72ceda514600563f2184e046b5ea1d55
sirozza <s.matviv@gmail.com>
2024-12-24 20:40:23 +0200

    241224_01

---
+++ 26. Remove Duplicates from Sorted Array/01.py
@@ -0,0 +1,13 @@
+def removeDuplicates(nums):
+    nums2 = []
+    for i in range(len(nums)):
+        if len(nums) == 1:
+            nums2.append(i)
+        elif nums[]
+        elif nums[i] != nums[i+1]:
+            nums2.append(i)
+
+
+a = [1,1,2]
+
+print(removeDuplicates(a))
