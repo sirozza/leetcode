1cc0706664c380d25160dca296cac8db8d355864
sirozza <s.matviv@gmail.com>
2025-01-02 17:08:49 +0200

    020125_02

---
+++ test_class/metodu_klassa.py
@@ -0,0 +1,16 @@
+
+class Point:
+    color = 'red'
+    circle = 2
+
+    def set_coords(self, x, y):
+        self.x = x
+        self.y = y
+
+class Point2:
+    x = 0
+    y = 0
+
+pt = Point()
+pt.set_coords(1, 3)
+print(pt.__dict__)
