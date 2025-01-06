6f859aed03fa714284b83095f3f86a95359ce362
sirozza <s.matviv@gmail.com>
2025-01-06 11:24:26 +0200

    060125_01

--- test_class/fio_11.py
+++ test_class/fio_11.py
@@ -1,13 +1,87 @@
+from string import ascii_letters
+
 
 class Person:
-    def __init__(self, fio, old, ps, weight):
-        self.__fio = fio.split()
-        self.__old = old
-        self.__passport = ps
-        self.__weight = weight
+    S_RUS = 'абвгдеёжзийклмнопрстуфхцчшщьъэюя-'
+    S_RUS_UPPER = S_RUS.upper()
 
+    def __init__(self, fio, old, ps, weight):
+        self.verify_fio(fio)
+
+        self.__fio = fio.split()
+        self.old = old
+        self.passport = ps
+        self.weight = weight
 
     @classmethod
     def verify_fio(cls, fio):
         if type(fio) != str:
             raise TypeError('ФИО должно быть строкой')
+        f = fio.split()
+        if len(f) != 3:
+            raise TypeError('Не верный формат ФИО')
+
+        letters = ascii_letters + cls.S_RUS + cls.S_RUS_UPPER
+        for s in f:
+            if len(s) < 1:
+                raise TypeError('В ФИО должен быть хотя бы один символ')
+            if len(s.strip(letters)) != 0:
+                raise TypeError('В ФИО можно использовать только буквенные символы и дефис')
+
+    @classmethod
+    def verify_old(cls, old):
+        if type(old) != int or old < 14 or old > 120:
+            raise TypeError('Возраст должен быть целым числом в диапазоне [14; 120]')
+
+    @classmethod
+    def verify_weight(cls, w):
+        if type(w) != float or w < 20:
+            raise TypeError('Вес должен быть вещественным числом от 20 и выще')
+
+    @classmethod
+    def verify_ps(cls, ps):
+        if type(ps) != str:
+            raise TypeError('Паспорт должен быть строкой')
+
+        s = ps.split()
+        if len(s) != 2 or len(s[0]) != 4 or len(s[1]) != 6:
+            raise TypeError('Неверный формат паспорта')
+
+        for p in s:
+            if not p.isdigit():
+                raise TypeError('Серия и номер паспорта должен быть числом')
+
+    @property
+    def fio(self):
+        return self.__fio
+
+    @property
+    def old(self):
+        return self.__old
+
+    @old.setter
+    def old(self, old):
+        self.verify_old(old)
+        self.__old = old
+
+    @property
+    def weight(self):
+        return self.__weight
+
+    @weight.setter
+    def weight(self, weight):
+        self.verify_weight(weight)
+        self.__weight = weight
+
+    @property
+    def passport(self):
+        return self.__passport
+
+    @passport.setter
+    def passport(self, passport):
+        self.verify_ps(passport)
+        self.__passport = passport
+
+p = Person('Матвиив Сергей Николаевич', 47, '1234 567890', 72.0)
+p.old = 35
+print(p.__dict__)
